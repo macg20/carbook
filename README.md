0e39cdb4be568d1a6c3e2f942e64c356d5090bf6
macg20 <kossss1@o2.pl>
2018-06-30 23:22:40 +0200

    add newest feedstock test add readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# Carbook
+
+Carbook is simple Spring Cloud microservices application of car service book.
+
+Modules: 
+  - feedstock-service
+  - discrovery-sercice
+  - commons
+  - carbook-service(future)
+  - gateway-zulu(future)
+  - uaa-service(future)
+  
+
+
--- feedstock-service/src/test/java/pl/emgie/carbook/feedstockservice/test/FeedstockCustomRepositoryTest.java
+++ feedstock-service/src/test/java/pl/emgie/carbook/feedstockservice/test/FeedstockCustomRepositoryTest.java
@@ -6,20 +6,21 @@ import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
 import pl.emgie.carbook.feedstockservice.domain.FeedstockEntity;
-import pl.emgie.carbook.feedstockservice.repositories.FeedstockCustomRepository;
 import pl.emgie.carbook.feedstockservice.repositories.FeedstockRepository;
 import pl.emgie.carbook.feedstockservice.utils.FeedstockType;
 
 import javax.transaction.Transactional;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static pl.emgie.carbook.feedstockservice.test.helpers.FeedstockPriceConstant.*;
 
 
@@ -50,16 +51,42 @@ public class FeedstockCustomRepositoryTest extends AbstractTest {
     }
 
     @Test
-    public void findNewestFeedstockDataTest() {
+    public void findMockNewestFeedstockDataTest() {
 
         when(mockRepository.findNewestFeedstocksEntity()).thenReturn(createNewestFeedstocksCollection());
 
         List<FeedstockEntity> result = mockRepository.findNewestFeedstocksEntity();
         verify(mockRepository).findNewestFeedstocksEntity();
-        verify(mockRepository,times(1)).findNewestFeedstocksEntity();
+        verify(mockRepository, times(1)).findNewestFeedstocksEntity();
 
         assertThat(result.size()).isEqualTo(4);
     }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void findNewestFeedstockDataTest() {
+        // given
+        List<FeedstockEntity> entities = createNewestData();
+        repository.saveAll(entities);
+
+        //when
+        List<FeedstockEntity> result = repository.findNewestFeedstocksEntity();
+        List<FeedstockType> resultType = result.stream().map(x -> x.getType()).collect(Collectors.toList());
+
+
+        //then
+        assertThat(result.size()).isEqualTo(4);
+        Arrays
+                .stream(FeedstockType.values())
+                .forEach(
+                        x -> assertThat(resultType.contains(x)).isTrue()
+                );
+        result.forEach(x -> {
+            assertThat(x.getCreateDate().toLocalDate()).isEqualTo(LocalDate.now());
+        });
+    }
+
 
     private List<FeedstockEntity> createNewestFeedstocksCollection() {
         return Arrays
@@ -68,28 +95,61 @@ public class FeedstockCustomRepositoryTest extends AbstractTest {
                         createMockFeedstockEntityWithDieselType(),
                         createMockFeedstockEntityWithPetrolType(),
                         createMockFeedstockEntityWithGasType()
-                        );
+                );
     }
 
 
     private FeedstockEntity createMockFeedstockEntityWithOilType() {
-        return createFeedstockEntityByTypeAndPrice(FeedstockType.OIL, OIL_PRICE );
+        return createFeedstockEntityByTypeAndPrice(FeedstockType.OIL, OIL_PRICE);
     }
 
     private FeedstockEntity createMockFeedstockEntityWithDieselType() {
-        return createFeedstockEntityByTypeAndPrice(FeedstockType.DIESEL, DIESEL_PRICE );
+        return createFeedstockEntityByTypeAndPrice(FeedstockType.DIESEL, DIESEL_PRICE);
     }
 
     private FeedstockEntity createMockFeedstockEntityWithPetrolType() {
-        return createFeedstockEntityByTypeAndPrice(FeedstockType.PETROL, PETROL_PRICE );
+        return createFeedstockEntityByTypeAndPrice(FeedstockType.PETROL, PETROL_PRICE);
     }
 
     private FeedstockEntity createMockFeedstockEntityWithGasType() {
-        return createFeedstockEntityByTypeAndPrice(FeedstockType.GAS, GAS_PRICE );
+        return createFeedstockEntityByTypeAndPrice(FeedstockType.GAS, GAS_PRICE);
+    }
+
+    private List<FeedstockEntity> createNewestData() {
+        List<FeedstockEntity> entities = new ArrayList<>();
+        entities.addAll(createNewestFeedstocksCollection());
+        entities.addAll(createNewestFeedstocksYesterdayCollection());
+        entities.addAll(createNewestFeedstocksDayBeforeYesterdayCollection());
+        return entities;
+    }
+
+    private List<FeedstockEntity> createNewestFeedstocksYesterdayCollection() {
+        List<FeedstockEntity> yesterdayCollection = Arrays
+                .asList(
+                        createMockFeedstockEntityWithOilType(),
+                        createMockFeedstockEntityWithDieselType(),
+                        createMockFeedstockEntityWithPetrolType(),
+                        createMockFeedstockEntityWithGasType()
+                );
+        yesterdayCollection.forEach(entity -> entity.setCreateDate(LocalDateTime.now().minus(1, ChronoUnit.DAYS)));
+        return yesterdayCollection;
+    }
+
+    private List<FeedstockEntity> createNewestFeedstocksDayBeforeYesterdayCollection() {
+        List<FeedstockEntity> dayBeforeYesterdayCollection = Arrays
+                .asList(
+                        createMockFeedstockEntityWithOilType(),
+                        createMockFeedstockEntityWithDieselType(),
+                        createMockFeedstockEntityWithPetrolType(),
+                        createMockFeedstockEntityWithGasType()
+                );
+        dayBeforeYesterdayCollection.forEach(entity -> entity.setCreateDate(LocalDateTime.now().minus(2, ChronoUnit.DAYS)));
+        return dayBeforeYesterdayCollection;
+
     }
 
     private FeedstockEntity createFeedstockEntityByTypeAndPrice(FeedstockType type, BigDecimal price) {
-       return FeedstockEntity
+        return FeedstockEntity
                 .builder()
                 .type(type)
                 .price(price)
