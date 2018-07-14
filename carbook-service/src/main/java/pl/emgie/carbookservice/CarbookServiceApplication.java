874f40a893496d7c35e4396d6ce621b3c27e9ef5
macg20 <kossss1@o2.pl>
2018-07-14 19:02:36 +0200

    add new module

---
+++ carbook-service/src/main/java/pl/emgie/carbookservice/CarbookServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.emgie.carbookservice;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarbookServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarbookServiceApplication.class, args);
+    }
+}
